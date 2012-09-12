5dee3bacff14c587dd795ddb6d5c0df5cba1ff66
Dave Walk <daviddwalk@gmail.com>
2012-09-11 22:58:42 -0400

    Returning JSON!

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 import json
 from flask import Flask
 from flask import request
+from flask import Response
 from pymongo import Connection
 from bson import json_util
 from urlparse import urlparse
@@ -19,30 +20,40 @@ def getstates():
     filter = request.args.get('require', '')
     if (filter):
         states = db.states.find({'vote.' + filter: True}, { '_id': False })
-        return str(json.dumps({'results': list(states)},
+        data= str(json.dumps({'results': list(states)},
             default = json_util.default,
             indent=4))
+        response = Response(data, status=200, mimetype='application/json')
+        return response
+
     else:
-        states = db.states.find({}, { '_id': False })
-        return str(json.dumps({'results' : list(states)}, 
-            default = json_util.default,
-            indent=4))
+        states = db.states.find({}, { '_id' : False })
+        data = str(json.dumps({'results': list(states)},
+                default = json_util.default,
+                indent=4))
+        response = Response(data, status=200, mimetype='application/json')
+        return response
 
 @app.route("/states/<statename>")
 def getstate(statename):
     try:
         if len(statename) == 2:
             state = db.states.find({'info.abbr': statename.upper()}, { '_id': False })
-            return str(json.dumps({'results': list(state)},
+            data = str(json.dumps({'results': list(state)},
                 default = json_util.default,
                 indent=4))
+            response = Response(data, status=200, mimetype='application/json')
+            return response
         else:
             state = db.states.find({'name': statename.title()}, { '_id': False })
-            return str(json.dumps({'results': list(state)},
+            data = str(json.dumps({'results': list(state)},
                 default = json_util.default,
                 indent=4))
+            response = Response(data, status=200, mimetype='application/json')
+            return response
+
     except Exception as err:
         return str(err)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
